8f62bc6ff887bfa57500f3dcad92bc022888d295
Heraya <herayafpm@gmail.com>
2019-05-23 11:47:51 +0700

    my-react app first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items : []
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => this.setState({items:data}))
+  }
+
+  render(){
+    const {items,error} = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {items.map((item,index)=><li key={index}>{item.name}</li>)}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// function getName() {
+//     return "Heraya haahh";
+// }
+// const nama = "HAHAHAH";
+// const element = <h1>Hello {getName()}</h1>;
+// ReactDOM.render(element,document.getElementById('master'));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
