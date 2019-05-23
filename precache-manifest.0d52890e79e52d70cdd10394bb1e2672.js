12934bd02e1726104648fd621e05c57e671f1abb
Heraya <herayafpm@gmail.com>
2019-05-23 11:57:39 +0700

    Updates

---
+++ precache-manifest.0d52890e79e52d70cdd10394bb1e2672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234655d756e80356cc7e41ccf88b258c",
+    "url": "raydragneel.github.io/my-react-app/index.html"
+  },
+  {
+    "revision": "d87a3aa3a388114764f5",
+    "url": "raydragneel.github.io/my-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "raydragneel.github.io/my-react-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d87a3aa3a388114764f5",
+    "url": "raydragneel.github.io/my-react-app/static/js/main.0f9ebd76.chunk.js"
+  },
+  {
+    "revision": "589e1994d3541897b782",
+    "url": "raydragneel.github.io/my-react-app/static/js/runtime~main.5b9f12a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "raydragneel.github.io/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
